c8bfc6b5de6049e02d2f628240f9bcb712f58737
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2023-01-04 20:08:03 +1100

    Initial commit

---
+++ src/components/SurveyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <survey :survey="survey" />
+</template>
+<script>
+import Vue from "vue";
+import { Model, StylesManager } from "survey-vue";
+import "survey-vue/defaultV2.min.css";
+import "./index.css";
+import { json } from "./json";
+
+StylesManager.applyTheme("defaultV2");
+
+export default Vue.component("survey-component", {
+  name: "survey-component",
+  data() {
+    const survey = new Model(json);
+    return {
+      survey: survey,
+    };
+  },
+});
+</script>
